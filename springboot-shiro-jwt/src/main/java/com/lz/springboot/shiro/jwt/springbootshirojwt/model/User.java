337907b9182b64d495e2210aa3012d117a157ab1
李哲 <querybyljs@163.com>
2019-04-14 20:55:54 +0800

    #添加

---
+++ springboot-shiro-jwt/src/main/java/com/lz/springboot/shiro/jwt/springbootshirojwt/model/User.java
@@ -0,0 +1,30 @@
+package com.lz.springboot.shiro.jwt.springbootshirojwt.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @author 李哲 【wb71@yingu.com】
+ * @date 2019/4/14 20:12
+ */
+@Getter
+@Setter
+@ToString
+@Data
+@Entity
+@Table(name = "t_user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
---
+++ springboot-shiro-jwt/src/main/resources/db/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO t_user VALUES ('lizhe','123abc');
+INSERT INTO t_user VALUES ('zhangsan','123abc');
