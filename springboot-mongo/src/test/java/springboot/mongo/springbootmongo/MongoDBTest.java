4ff59a2ae9c81c21e74b319426c5b40edf681bd5
lizhe <lizhej@enn.cn>
2019-04-29 16:12:05 +0800

    #spring boot mongo db study

---
+++ springboot-mongo/src/test/java/springboot/mongo/springbootmongo/MongoDBTest.java
@@ -0,0 +1,56 @@
+package springboot.mongo.springbootmongo;
+
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ClassName: MongoDBTest
+ * Description:
+ * Author: LiZhe lizhej@enn.cn
+ * Date: 2019/4/29 15:30
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class MongoDBTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    public void findAllUsersTest() {
+        List<Map> users = this.mongoTemplate.findAll(Map.class, "users");
+        if (users != null && !users.isEmpty()) {
+            users.forEach(x->{
+                System.out.println(x.toString());
+            });
+        }
+    }
+
+    @Test
+    public void insertOneTest(){
+
+        Map<String,Object> insert =Maps.newHashMap();
+        insert.put("contact","15801372602");
+        insert.put("dob","1992-11-11");
+        insert.put("name","dazhe");
+
+        Map<String, Object> user = this.mongoTemplate.insert(insert, "user");
+
+        System.out.println(user.get("_id"));
+    }
+
+}
