b09bd1bcafa4d2d9850e3385e01b8820b4379afc
lizhe <lizhej@enn.cn>
2019-11-04 11:02:41 +0800

    #

---
+++ theard-study/src/main/java/com/study/JMH/JMHSample_01_HelloWorld.java
@@ -0,0 +1,36 @@
+package com.study.JMH;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName: JMHSample_01_HelloWorld
+ * @Description:
+ * @Author: LiZhe lizhej@enn.cn
+ * @Date: 2019/10/28 17:15
+ * @History: <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class JMHSample_01_HelloWorld {
+
+    @Benchmark
+    public void welHelloWorldThere(){
+        // this method was intentionally left blank.
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder().include(JMHSample_01_HelloWorld.class.getSimpleName()).forks(1).build();
+        new Runner(opt).run();
+    }
+}
